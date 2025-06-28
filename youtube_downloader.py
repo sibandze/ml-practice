17f6a548009ffc4912049b8c55d3d15940f3d9c9
Nkosingiphile Simnene Sibandze <56678793+sibandze@users.noreply.github.com>
2025-06-28 11:05:01 +0200

    Update youtube_downloader.py

--- youtube_downloader.py
+++ youtube_downloader.py
@@ -18,12 +18,12 @@ def download_video_content(title, temp_dir, output_dir, url):
     try:
         with YOUTUBE.YoutubeDL(ydl_opts) as ydl:
             print()
-            print(f" Downloading: {title}")
+            print(f"   Downloading: {title}")
             print()
             ydl.download([url])
             post_processed(temp_dir, output_dir)
     except Exception as e:
-        print(f"Error downloading video: {e}")
+        print(f"   Error downloading video: {e}")
         
 def download_video(url, output_dir):
     title = get_filename(url)
@@ -46,12 +46,12 @@ def get_filename(url):
 def progress_hook(d, temp_dir, output_dir):
     if d['status'] == 'downloading':
         percent = d.get('downloaded_bytes', 0) / d.get('total_bytes', 1)
-        print(f"\rDownloading: {percent*100:.2f}%", end='')
+        print(f"\r   Downloading: {percent*100:.2f}%", end='')
     elif d['status'] == 'finished':
-        print("\nPartial download finished. Finalizing...")
+        print("\n   Partial download finished. Finalizing...")
             
 def post_processed(temp_dir, output_dir):
-        print("\nPostprocessing complete. Moving to output directory...")
+        print("\n   Postprocessing complete. Moving to output directory...")
         try:        
             for file in os.listdir(temp_dir):
                 if file != ".url" and file != ".title":
@@ -59,23 +59,23 @@ def post_processed(temp_dir, output_dir):
             os.remove(os.path.join(temp_dir, ".url"))
             os.remove(os.path.join(temp_dir, ".title"))
             os.rmdir(temp_dir)
-            print("Download complete.")
+            print("   Download complete.")
         except Exception as e:
-            print(f"Error moving files: {e}")
+            print(f"   Error moving files: {e}")
     
     
 
 def resume_download(output_dir):
     temp_dirs = [d for d in os.listdir(DOWNLOAD_DIR) if os.path.isdir(os.path.join(DOWNLOAD_DIR, d))]
     if not temp_dirs:
-        print("No partial downloads found.")
+        print("   No partial downloads found.")
         return
     titles = []
     for i, temp_dir in enumerate(temp_dirs):
         title = open(os.path.join(DOWNLOAD_DIR,temp_dir, ".title"), "r").read().strip()
         titles.append(title)
-        print(f"{i+1}. {title}")
-    choice = input("Enter the number of the download to resume, or 'all' to resume all: ")
+        print(f"   {i+1}. {title}")
+    choice = input("   Enter the number of the download to resume, or 'all' to resume all: ")
     if not choice or choice.lower() == 'all':
         for idx, temp_dir in enumerate(temp_dirs):
             temp_dir = os.path.join(DOWNLOAD_DIR, temp_dir)
@@ -89,9 +89,9 @@ def resume_download(output_dir):
                 url = open(os.path.join(temp_dir, ".url"), "r").read().strip()
                 download_video_content(titles[choice-1], temp_dir, output_dir, url)
             else:
-                print("Invalid choice.")
+                print("   Invalid choice.")
         except ValueError:
-            print("Invalid input.")
+            print("   Invalid input.")
 
 def main():
     os.makedirs(DOWNLOAD_DIR, exist_ok=True)
